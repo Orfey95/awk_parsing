6f5fa63bedf78c291855a5ab1f29062193b1b45b
Sasha <sasha7692@gmail.com>
2020-03-25 23:34:35 +0200

    Update task_8.sh

--- task_8.sh
+++ task_8.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+
+# Variables
+log=$1
+
+# Code
+awk 'match($0,/(http|https):\/\/.+" "/) {print substr($0,RSTART,RLENGTH-1)}' $log | awk '{print $1}' | awk -F "/|)|\"" '{print $3}' | sort | uniq -c | sort -nr
