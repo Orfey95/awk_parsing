2317a4bd141fb9d328f62fdcf677ec55415fe153
Sasha <sasha7692@gmail.com>
2020-03-25 18:11:16 +0200

    Create task_5.sh

---
+++ task_5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Variables
+log=$1
+N=$2
+
+# The list of links with the minimum length
+echo "The list of links with the minimum length:"
+awk 'match($0,/GET .+ HTTP/) {print substr($0,RSTART,RLENGTH)}' $log | awk '{print $2}' | grep -ve '^/$' | sort | uniq -c | awk '{ print length, $0 }' | sort -ns | awk '{print $2,$3}' | head -n $N | awk '{ print length($2), $0 }'
+
+
+# The list of links with the maximum length
+echo "The list of links with the minimum length:"
+awk 'match($0,/GET .+ HTTP/) {print substr($0,RSTART,RLENGTH)}' $log | awk '{print $2}' | grep -ve '^/$' | sort | uniq -c | awk '{ print length, $0 }' | sort -nrs | awk '{print $2,$3}' | head -n $N | awk '{ print length($2), $0 }'
